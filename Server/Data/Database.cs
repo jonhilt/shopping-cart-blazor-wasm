5af6c2d8b58b42c210e69f6cbe194e6059100d1e
Jon Hilton <hilton.jon@gmail.com>
2020-06-24 11:07:11 +0100

    Updated data seeding to re-populate cart when items run out!

--- Server/Data/Database.cs
+++ Server/Data/Database.cs
@@ -13,30 +13,39 @@ namespace ShoppingCartStarter.Server.Data
         /// <summary>
         /// Seed dummy cart data
         /// </summary>
-        /// <param name="app"></param>
-        /// <param name="env"></param>
         public static void InitialiseDatabase(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-            using (var context = serviceScope.ServiceProvider.GetRequiredService<StoreContext>())
-            {
-                context.Database.Migrate();
+            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
+            using var context = serviceScope.ServiceProvider.GetRequiredService<StoreContext>();
+            
+            context.Database.Migrate();
 
-                if (!context.Carts.Any())
-                {
-                    var cart = new ShoppingCart
-                    {
-                        LineItems = new List<LineItem>
-                        {
-                            new LineItem{ Image = "test-image.jpg", Name="Big T-shirt", Price=39.50m, Quantity = 2 },
-                            new LineItem{ Image = "test-image.jpg", Name="Small White T-shirt", Price=19.50m, Quantity = 1 },
-                            new LineItem{ Image = "test-image.jpg", Name="Smart Speaker", Price=23.00m, Quantity = 1 },
-                        }
-                    };
-                    context.Carts.Add(cart);
-                    context.SaveChanges();
-                }
+            ShoppingCart cart;
+            if (context.Carts.Any())
+            {
+                cart = context.Carts
+                    .Include(x=>x.LineItems)
+                    .FirstOrDefault();
             }
+            else
+            {
+                cart = new ShoppingCart();
+                context.Carts.Add(cart);
+            }
+
+            if (cart?.LineItems.Count == 0)
+            {
+                cart.LineItems.AddRange(new[]
+                {
+                    new LineItem {Image = "test-image.jpg", Name = "Big T-shirt", Price = 39.50m, Quantity = 2},
+                    new LineItem {Image = "test-image.jpg", Name = "Small White T-shirt", Price = 19.50m, Quantity = 1},
+                    new LineItem {Image = "test-image.jpg", Name = "Smart Speaker", Price = 23.00m, Quantity = 1},
+                    new LineItem {Image = "test-image.jpg", Name = "Dumb Speaker", Price = 99.00m, Quantity = 3},
+                    new LineItem {Image = "test-image.jpg", Name = "Giraffe Poster", Price = 9.00m, Quantity = 20}
+                });
+            }
+
+            context.SaveChanges();
         }
     }
 }
