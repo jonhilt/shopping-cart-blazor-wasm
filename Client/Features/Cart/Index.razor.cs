f426b830fbebf88f687a9cf8419254752f01957e
Jon Hilton <hilton.jon@gmail.com>
2024-11-21 13:36:07 +0000

    Testing following .NET 9 upgrade

---
+++ Client/Features/Cart/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShoppingCartStarter.Shared.Cart;
+using System.Net.Http.Json;
+
+namespace ShoppingCartStarter.Client.Features.Cart
+{
+    public partial class Index
+    {
+        [Inject] private HttpClient Http { get; set; }
+
+        protected Details.Model Model { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Model = await Http.GetFromJsonAsync<Details.Model>("api/cart");
+        }
+        
+    }
+}
